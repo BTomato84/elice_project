70aa734ca1ccbe1d9ba6a6929f5a129f7ed9fa34
yspark <youngsoo.park@yanolja.com>
2023-12-09 13:55:50 +0900

    Bool Extension 추가
    not 함수 추가

---
+++ elice_project/Common/Bool+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Bool+Extension.swift
+//  elice_project
+//
+//  Created by 영수 박 on 2023/12/09.
+//
+
+import Foundation
+
+extension Bool {
+    func not() -> Bool {
+        return !self
+    }
+}
