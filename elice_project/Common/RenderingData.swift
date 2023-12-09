415cbc8059e9a480563b14a0f40020a65e2b6261
yspark <youngsoo.park@yanolja.com>
2023-12-09 10:35:14 +0900

    RenderingData 구축
    UIView RenderingData 구축
    UILabel RenderingData 구축
    UIImageView RenderingData 구축
    UIButton RenderingData 구축

---
+++ elice_project/Common/RenderingData.swift
@@ -0,0 +1,106 @@
+//
+//  RenderingData.swift
+//  elice_project
+//
+//  Created by 영수 박 on 2023/12/09.
+//
+
+import Foundation
+import UIKit
+
+struct ViewRenderingData : Equatable {
+    var borderWidth : CGFloat
+    var borderColor : UIColor
+    var cornerRadius : CGFloat
+    var contentMode : UIView.ContentMode
+    var isHidden : Bool
+
+    static var `default` : ViewRenderingData {
+        ViewRenderingData(borderWidth: 0.0, borderColor: .clear, cornerRadius: 0.0, contentMode: .topLeft, isHidden: false)
+    }
+}
+
+extension UIView {
+    func setBorder(width: CGFloat, color: UIColor) {
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = width
+    }
+
+    func setCornerRadius(_ radius: CGFloat) {
+        self.layer.cornerRadius = radius
+    }
+
+    func render(_ data: ViewRenderingData) {
+        setBorder(width: data.borderWidth, color: data.borderColor)
+        setCornerRadius(data.cornerRadius)
+        isHidden = data.isHidden
+        contentMode = data.contentMode
+    }
+}
+
+struct LabelRenderingData : Equatable {
+    var font : UIFont
+    var text : String
+    var color : UIColor
+    var textAlign : NSTextAlignment
+    var view : ViewRenderingData
+
+    init(font: UIFont, text: String, color: UIColor, textAlign: NSTextAlignment = .left, view: ViewRenderingData = .default) {
+        self.font = font
+        self.text = text
+        self.color = color
+        self.textAlign = textAlign
+        self.view = view
+    }
+}
+
+extension UILabel {
+    func render(_ data: LabelRenderingData) {
+        self.font = data.font
+        self.text = data.text
+        self.textColor = data.color
+        self.textAlignment = data.textAlign
+        self.render(data.view)
+    }
+}
+
+struct ImageRenderingData : Equatable {
+    var imageName : String
+    var view : ViewRenderingData
+
+    init(imageName: String, view: ViewRenderingData = .default) {
+        self.imageName = imageName
+        self.view = view
+    }
+}
+
+extension UIImageView {
+    func render(_ data: ImageRenderingData) {
+        self.image = UIImage(named: data.imageName)
+        self.render(data.view)
+    }
+}
+
+struct ButtonRenderingData : Equatable {
+    var buttonImageName : String?
+    var buttonTitle : String?
+    var view : ViewRenderingData
+
+    init(buttonImageName: String, buttonTitle: String?, view: ViewRenderingData = .default) {
+        self.buttonImageName = buttonImageName
+        self.buttonTitle = buttonTitle
+        self.view = view
+    }
+}
+
+extension UIButton {
+    func render(_ data: ButtonRenderingData) {
+        let image : UIImage? = {
+            guard let imageName = data.buttonImageName else { return nil }
+            return UIImage(named: imageName)
+        }()
+        self.setImage(image, for: .normal)
+        self.setTitle(data.buttonTitle, for: .normal)
+        self.render(data.view)
+    }
+}
